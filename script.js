33cc3ec2c38ef2d9390e7d4d05d0eb96193f54d6
uls19 <uls19@mail.ru>
2023-07-26 22:59:18 +0200

    All KREON desktop 1024-3840

--- script.js
+++ script.js
@@ -38,3 +38,34 @@ rollSlider();
 function rollSlider(){
     sliderLine.style.transform = 'translate(-'+count*width+'px)';
 }
+
+
+let reg = /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/;
+let inp = document.querySelector('.subscription__mail');
+let span = document.querySelector('.subscription__error');
+
+
+document.querySelector('.subscription__button').onclick = function(e){
+  e.preventDefault();
+
+  if (!validate(reg, inp.value)) {
+   notValid(inp, span, 'Проверка не пройдена');
+  }else{
+    valid(inp, span, '');
+  }
+};
+
+function validate(regex, inp){
+  return regex.test(inp);
+}
+
+function notValid(inp, el, mess){
+  inp.classList.add('is-invalid');
+  el.innerHTML = mess;
+}
+
+function valid(inp, el, mess){
+  inp.classList.remove('is-invalid');
+  inp.classList.add('is-valid');
+  el.innerHTML = mess;
+}
